c880af88a8713b00c0727588d7d5effdffb4e8bf
nilsec <nils.eckstein@googlemail.com>
2020-11-03 16:09:56 -0500

    Add id column to pos

--- synistereq/utils.py
+++ synistereq/utils.py
@@ -1,6 +1,6 @@
 import pandas
 
-def read_skid_csv(csv_path):
+def read_skids_csv(csv_path):
     """
     The csv needs to have one column called 'skid'
     """
@@ -9,15 +9,24 @@ def read_skid_csv(csv_path):
     skids = list(set([int(skid) for skid in skids]))
     return skids
 
-def read_position_csv(csv_path):
+def read_positions_csv(csv_path):
     """
-    The csv needs to have three columns called "x", "y", "z"
+    The csv needs to have three columns called "id", "x", "y", "z"
     """
     data = pandas.read_csv(csv_path)
     x_list = data["x"].to_list()
     y_list = data["y"].to_list()
     z_list = data["z"].to_list()
+    position_ids = data["id"].to_list()
 
     positions = [(z,y,x) for z,y,x in zip(z_list, y_list, x_list)]
-    return positions
+    return positions, position_ids
+
+def format_predictions(nt_probabilities, positions, position_ids, ids_to_skids):
+    pass
+
+
+
+
+
 
